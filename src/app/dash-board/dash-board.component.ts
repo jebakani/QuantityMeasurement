b470d1414ce2f72d507c3b1f747f9240c50e6842
Jebakani <vjishwarya@gmail.com>
2021-10-12 15:55:38 +0530

    [JEBAKANI]Add: Adding the card for measurement

--- src/app/dash-board/dash-board.component.ts
+++ src/app/dash-board/dash-board.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashBoardComponent implements OnInit {
 
   constructor() { }
-
+  measure='length';
   ngOnInit(): void {
   }
   someList=[
